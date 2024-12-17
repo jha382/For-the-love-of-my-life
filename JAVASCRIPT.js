83701414bb398e8ab29b360d5b6b4c96dbf7bbd4
jha382 <jhaja2089@gmail.com>
2024-12-17 16:33:32 +0800

    Add files via upload

---
+++ JAVASCRIPT.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html> 
+<Title> HI! <title>
+  <script text="text/Keep Pressing Ok!">
+  alert("I hate you..") 
+  alert ("I really do hate you")
+  alert ("But why is it so different")
+  alert ("When im with you?")
+  alert ("For some reason")
+  alert ("You've become my everything'")
+  alert ("But...")
+  alert ("I know i have no chance")
+  alert ("But please")
+  confirm("let me say this words to you")
+ 
+ 
+  <script> 
+  <head> 
+    
+    <body>
+      
+    <h1
+      style="color:Black;" When i first met of you in thought you were a bad person, someone who is always mad| and
+      someone distance himself from everyone but knowing you know makes it all different..Your voice i dont like it
+      The way you talk i hate it Your personality personally i hate it aswell i hate everything about you so much that
+      i love you more than i have ever loved someone in my years of living..When i tried to confess normally you told me
+      'that's just a joke right?' You made me promise that i was joking but to be honest i wasnt joking at all i dont like
+      what im feeling for you it's just so different and full of hatred then becomes to this thing called love I know you have 
+      a lover and by the time im sending you this my feelings are probably over you, im only sending is to let you know that i
+      loved you so much more than i love myself at this point, Ah and don't worry about me i can manage to forget about everything
+      i dont wanna be in a relationship with you but please let me admire and love you on my on i wanna experience this thing 
+      called adoration and admiration i'm probably known for having so many man but thats not me trust me im a fool when i REALLY 
+      Falled inlove just by doing this for a confession is already making me a fool, but i will do anything for a smile on your 
+      weird ass face haha im joking i love you so much </h1>
+      
+       
+     <img
+     src="https://pin.it/3Aykzjz4z 
